ce130fae9dd06933ad83d6fcf4660bd167c071f5
PEDDAPALEM SHAIK MAHAMMED SIRAJUDDIN <jarismohammed7@gmail.com>
2026-06-24 22:07:11 +0530

    Add files via upload

--- Counter App/index.js
+++ Counter App/index.js
@@ -1,1 +1,14 @@
-..
+import { useState } from "react";
+
+function App() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <>
+            <h2>{count}</h2>
+            <button onClick={() => setCount(count + 1)}>Increment</button>
+        </>
+    );
+}
+
+export default App;
