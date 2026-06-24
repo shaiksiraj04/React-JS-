9ede1e6fec326140d5248a58160e2a0421bc505f
PEDDAPALEM SHAIK MAHAMMED SIRAJUDDIN <jarismohammed7@gmail.com>
2026-06-24 22:12:28 +0530

    Add files via upload

--- Toggle Test/index.js
+++ Toggle Test/index.js
@@ -1,1 +1,13 @@
-..
+import { useState } from "react";
+
+function App() {
+    const [show, setShow] = useState(true);
+
+    return (
+        <>
+            <button onClick={() => setShow(!show)}>Toggle</button>
+            {show && <h1>Hello World</h1>}
+        </>
+    );
+}
+export default App;
