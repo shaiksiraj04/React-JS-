9462067fd40c0bbc269014d7b829e97872f4350a
PEDDAPALEM SHAIK MAHAMMED SIRAJUDDIN <jarismohammed7@gmail.com>
2026-06-24 22:10:25 +0530

    Add files via upload

--- Decrement Counter/index.js
+++ Decrement Counter/index.js
@@ -1,1 +1,13 @@
-..
+import { useState } from "react";
+
+function App() {
+    const [count, setCount] = useState(10);
+
+    return (
+        <>
+            <h2>{count}</h2>
+            <button onClick={() => setCount(count - 1)}>Decrement</button>
+        </>
+    );
+}
+export default App;
